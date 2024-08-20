d08835eeffd27149d1b159a79b6d81a9cbb14246
leticiaexe <143099139+leticiaexe@users.noreply.github.com>
2024-08-20 16:25:11 -0300

    historia joguinho

--- script.js
+++ script.js
@@ -6,17 +6,40 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "vc acorda com uma mensagem em seu celular, nela estava escrito escolha ou morra , oq vc escolhe?",
         alternativa:[
-            "Alternativa 1",
-            "Alternativa 2"
+            "eu escolho",
+            "eu morro"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "vc escolheu a primeira alternativa agora vc deve escolher para nao morrer",
         alternativa:[
-            "Alternativa 1",
-            "Alternativa 2"
+            "um dedo do pe",
+            "morrer"
         ]
-    }
+    },
+    {
+        enunciado: "vc escolheu um dedo do pe, agora tera de secolher novamente para n morrer",
+        alternativa:[
+            "uma mao",
+            "morrer"
+
+        ]
+    },
+    {
+        enunciado: "vc escolheu morrer, mas para sua sorte pode reviver em um lugar",
+        alternativa:[
+            "ceu",
+            "inferno"
+        ]
+    },
+    {
+        enunciado: "vc escolheu o ceu",
+        alternativa:[
+            "vc acordou de um pesadelo",
+            "vc acordou de um pesadelo"
+        ]
+    },
+ 
 ];
