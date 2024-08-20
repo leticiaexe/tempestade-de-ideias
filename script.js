19426f0fd9a7d009d2901801d2ecff4feeb84f5a
leticiaexe <143099139+leticiaexe@users.noreply.github.com>
2024-08-20 16:35:55 -0300

    historinha

--- script.js
+++ script.js
@@ -43,3 +43,11 @@ const perguntas = [
     },
  
 ];
+let atual= 0;
+let perguntaAtual;
+
+function mostrarPergunta(){
+    perguntaAtual=perguntas[atual];
+    caixaPerguntas.textContent=perguntaAtual.enunciado
+}
+mostrarPergunta();
